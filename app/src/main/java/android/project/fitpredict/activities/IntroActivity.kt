58a394d8e84b252a406115e1b66600b40f26d694
mayankr1307 <mayankr20027@gmail.com>
2024-09-12 18:37:21 +0530

    Created IntroActivity.kt

--- app/src/main/java/android/project/fitpredict/activities/IntroActivity.kt
+++ app/src/main/java/android/project/fitpredict/activities/IntroActivity.kt
@@ -3,10 +3,28 @@ package android.project.fitpredict.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.project.fitpredict.R
+import android.project.fitpredict.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private var binding: ActivityIntroBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flSignUp?.setOnClickListener {
+
+        }
+
+        binding?.flSignIn?.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
