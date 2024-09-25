e1639beb9fdbb48121be8ca61f3842eac2f3b490
mayankr1307 <mayankr20027@gmail.com>
2024-09-25 11:27:19 +0530

    Created drawer layout in MainActivity.

--- app/src/main/java/android/project/fitpredict/activities/MainActivity.kt
+++ app/src/main/java/android/project/fitpredict/activities/MainActivity.kt
@@ -3,10 +3,39 @@ package android.project.fitpredict.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.project.fitpredict.R
+import android.project.fitpredict.databinding.ActivityInfoBinding
+import android.project.fitpredict.databinding.ActivityMainBinding
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.core.view.GravityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        window.statusBarColor = resources.getColor(R.color.variant2, null)
+
+        binding?.dlMain?.tbMain?.ivUser?.setOnClickListener {
+            binding?.dlMain?.dlDrawer?.openDrawer(GravityCompat.START)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (binding?.dlMain?.dlDrawer?.isDrawerOpen(GravityCompat.START) == true) {
+            super.onBackPressed()
+        } else {
+            binding?.dlMain?.dlDrawer?.openDrawer(GravityCompat.START)
+        }
+    }
+
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
